a2f870d1300853188929ad6aa26554b85bf72c28
Isaac Shea <isaac_shea@icloud.com>
2020-09-29 11:48:38 +1000

    Initial commit
    Testing visual content (incomplete)

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("fullPageNav").style.width = "100%";
+    document.getElementById("navContent").style.display = "flex";
+}
+
+function closeNav() {
+    document.getElementById("fullPageNav").style.width = "0%";
+    document.getElementById("navContent").style.display = "none";
+}
